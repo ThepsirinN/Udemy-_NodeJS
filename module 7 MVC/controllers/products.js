77a91a39d795c3844e34a33366aa32e99887d297
ThepsirinN <thepsirin.n@alumni.chula.ac.th>
2022-06-20 15:57:05 +0700

    finished learn module 7

--- module 7 MVC/controllers/products.js
+++ module 7 MVC/controllers/products.js
@@ -1,4 +1,6 @@
-const products = []
+const ProductModel = require('../models/product')
+
+/* const products = [] */
 
 exports.getAddProduct = (req, res, next)=>{
     console.log("/add-product")
@@ -6,8 +8,10 @@ exports.getAddProduct = (req, res, next)=>{
 }
 
 exports.postAddProduct = (req, res, next)=>{
-    console.log(req.body)
-    products.push({title : req.body.title})
+    /* console.log(req.body)
+    products.push({title : req.body.title}) */
+    const product = new ProductModel(req.body.title)
+    product.save()
     res.redirect('/')
 }
 
@@ -16,6 +20,10 @@ exports.getProduct = (req, res, next)=>{
     console.log(adminData.products)
     const products = adminData.products */
     // product can refer to array product
-    res.render('shop',{prods : products, docTitle: 'Shop', path:'/'})
-
+    //const products = ProductModel.fetchAll() // no neccessary to create new instance bcz of static method
+    
+    // products mean [] or JSON.parse(fileContent) in getProductFromFile bcz this function is eq to callback
+    ProductModel.fetchAll((products) => {
+        res.render('shop',{prods : products, docTitle: 'Shop', path:'/'}) 
+    })
 }
